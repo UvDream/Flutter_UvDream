51eecd7c5c189e2d4e20fbfd34c7290ea0e03308
wangzhongjie <wangzhongjie@ikuijia.com>
2019-01-25 14:08:36 +0800

    输入框

--- lib/pages/components/component.dart
+++ lib/pages/components/component.dart
@@ -6,6 +6,7 @@ import './router_animation/animation_list.dart';
 import './row_column/row_column.dart';
 import './drawer/drawer.dart';
 import './button/button.dart';
+import './text_field/text_field.dart';
 class ComponentPage extends StatelessWidget {
 
   @override
@@ -26,11 +27,13 @@ class ComponentPage extends StatelessWidget {
 class ComponentsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //  _pushNavigatorWidget(dynamic pages){
+    //  _pushNavigatorWidget(Widget pages){
     //    Navigator.push(
     //       context, 
     //       MaterialPageRoute(
-    //         builder:(context)=>pages()
+    //         builder:(context){
+    //           return pages;
+    //         }
     //       )
     //     );
     // }
@@ -122,6 +125,14 @@ void _onTop(BuildContext context, String id){
             )
         );
         break;
+          case "9":
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder:(context)=>new TextFieldPage()
+            )
+        );
+        break;
     }
 }
 
@@ -135,34 +146,34 @@ class Choice{
   final Object pages;
 }
 const List<Choice> ComponentsLists=const <Choice>[
-  const Choice(title:"导航栏组件",subtitle:"常见导航栏组件",indexs:"1",pages:BottomNavigation),
-  const Choice(title:"导航栏组件",subtitle:"中间嵌入的底部导航栏组件",indexs:"2",pages:BottomNavigationButton),
-  const Choice(title:"AppBar Tabs",subtitle:"悬浮在AppBar下面的tabs",indexs:"3",pages:BottomNavigation),
-  const Choice(title:"路由动画",subtitle:"常见的动画",indexs:"4",pages:BottomNavigation),
-  const Choice(title:"Row/Column",subtitle:"水平/垂直布局",indexs:"5",pages:BottomNavigation),
-  const Choice(title:"Drawer",subtitle:"侧边抽屉菜单栏",indexs:"6",pages:BottomNavigation),
-  const Choice(title:"Button",subtitle:"按钮",indexs:"7",pages:BottomNavigation),
-  const Choice(title:"PopupMenuButton",subtitle:"弹出式菜单列表",indexs:"8",pages:BottomNavigation),
-  const Choice(title:"ButtonBar",subtitle:"水平排列的按钮组",indexs:"8",pages:BottomNavigation),
-  const Choice(title:"TextField",subtitle:"文本输入框",indexs:"9",pages:BottomNavigation),
-  const Choice(title:"Checkbox",subtitle:"复选框",indexs:"10",pages:BottomNavigation),
-  const Choice(title:"Radio",subtitle:"单选框",indexs:"11",pages:BottomNavigation),
-  const Choice(title:"Switch",subtitle:"切换按钮",indexs:"12",pages:BottomNavigation),
-  const Choice(title:"Slider",subtitle:"滑块",indexs:"13",pages:BottomNavigation),
-  const Choice(title:"Date & Time Pickers",subtitle:"时间日期选择器",indexs:"14",pages:BottomNavigation),
-  const Choice(title:"SimpleDialog",subtitle:"简单的对话框",indexs:"15",pages:BottomNavigation),
-  const Choice(title:"AlertDialog",subtitle:"确认框",indexs:"16",pages:BottomNavigation),
-  const Choice(title:"BottomSheet",subtitle:"底部弹出框",indexs:"17",pages:BottomNavigation),
-  const Choice(title:"ExpansionPanel",subtitle:"未知",indexs:"18",pages:BottomNavigation),
-  const Choice(title:"SnackBar",subtitle:"轻量的在屏幕底部信息提示",indexs:"19",pages:BottomNavigation),
-  const Choice(title:"Image",subtitle:"图片组件",indexs:"20",pages:BottomNavigation),
-  const Choice(title:"Icon",subtitle:"小图标",indexs:"21",pages:BottomNavigation),
-  const Choice(title:"Chip",subtitle:"标签",indexs:"22",pages:BottomNavigation),
-  const Choice(title:"Tooltip",subtitle:"标签",indexs:"22",pages:BottomNavigation),
-  const Choice(title:"DataTable",subtitle:"标签",indexs:"22",pages:BottomNavigation),
-  const Choice(title:"Card",subtitle:"标签",indexs:"22",pages:BottomNavigation),
-  const Choice(title:"LinearProgressIndicator",subtitle:"标签",indexs:"22",pages:BottomNavigation),
-  const Choice(title:"Stepper",subtitle:"标签",indexs:"22",pages:BottomNavigation),
-  const Choice(title:"Divider",subtitle:"标签",indexs:"22",pages:BottomNavigation),
+  const Choice(title:"导航栏组件",subtitle:"常见导航栏组件",indexs:"1"),
+  const Choice(title:"导航栏组件",subtitle:"中间嵌入的底部导航栏组件",indexs:"2"),
+  const Choice(title:"AppBar Tabs",subtitle:"悬浮在AppBar下面的tabs",indexs:"3"),
+  const Choice(title:"路由动画",subtitle:"常见的动画",indexs:"4"),
+  const Choice(title:"Row/Column",subtitle:"水平/垂直布局",indexs:"5"),
+  const Choice(title:"Drawer",subtitle:"侧边抽屉菜单栏",indexs:"6"),
+  const Choice(title:"Button",subtitle:"按钮",indexs:"7"),
+  const Choice(title:"PopupMenuButton",subtitle:"弹出式菜单列表",indexs:"8"),
+  const Choice(title:"ButtonBar",subtitle:"水平排列的按钮组",indexs:"8"),
+  const Choice(title:"TextField",subtitle:"文本输入框",indexs:"9"),
+  const Choice(title:"Checkbox",subtitle:"复选框",indexs:"10"),
+  const Choice(title:"Radio",subtitle:"单选框",indexs:"11"),
+  const Choice(title:"Switch",subtitle:"切换按钮",indexs:"12"),
+  const Choice(title:"Slider",subtitle:"滑块",indexs:"13"),
+  const Choice(title:"Date & Time Pickers",subtitle:"时间日期选择器",indexs:"14"),
+  const Choice(title:"SimpleDialog",subtitle:"简单的对话框",indexs:"15"),
+  const Choice(title:"AlertDialog",subtitle:"确认框",indexs:"16"),
+  const Choice(title:"BottomSheet",subtitle:"底部弹出框",indexs:"17"),
+  const Choice(title:"ExpansionPanel",subtitle:"未知",indexs:"18"),
+  const Choice(title:"SnackBar",subtitle:"轻量的在屏幕底部信息提示",indexs:"19"),
+  const Choice(title:"Image",subtitle:"图片组件",indexs:"20"),
+  const Choice(title:"Icon",subtitle:"小图标",indexs:"21"),
+  const Choice(title:"Chip",subtitle:"标签",indexs:"22"),
+  const Choice(title:"Tooltip",subtitle:"标签",indexs:"22"),
+  const Choice(title:"DataTable",subtitle:"标签",indexs:"22"),
+  const Choice(title:"Card",subtitle:"标签",indexs:"22"),
+  const Choice(title:"LinearProgressIndicator",subtitle:"标签",indexs:"22"),
+  const Choice(title:"Stepper",subtitle:"标签",indexs:"22"),
+  const Choice(title:"Divider",subtitle:"标签",indexs:"22"),
 ];
 
---
+++ lib/pages/components/text_field/text_field.dart
@@ -0,0 +1,78 @@
+/*
+ * @Author: wangzhongjie
+ * @Date: 2019-01-25 10:28:43
+ * @LastEditors: wangzhongjie
+ * @LastEditTime: 2019-01-25 13:33:44
+ * @Description: 输入框
+ * @Email: uvdream@163.com
+ */
+import 'package:flutter/material.dart';
+class TextFieldPage extends StatelessWidget {
+  void _textFieldChanged(String str) {
+    print(str);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("输入框"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+           Divider(),
+           Row(
+                children: <Widget>[
+                  Text("输入框"),
+                  Expanded(
+                      child: TextField(
+                        // 为true的时候长按会显示系统粘贴板的内容
+                        enableInteractiveSelection: false,
+                        // 禁止输入
+                        enabled: true,
+                        // 聚焦点颜色
+                        cursorColor: Colors.red,
+                        //聚焦点圆边角
+                        cursorRadius: Radius.circular(10),
+                        // 聚焦点宽度
+                        cursorWidth: 1,
+                        // 自动获取焦点
+                        autofocus: false,
+                        // 长度限制
+                        // maxLength: 5,
+                        // 是否为密码
+                        obscureText: false,
+                        // 输入框类型
+                        keyboardType:TextInputType.number,
+                        // 文本对齐方式
+                        textAlign: TextAlign.center,
+                        // 最大行数
+                        maxLines: 1,
+                        // 是否自动更正
+                        autocorrect:true,
+                        // 值改变事件
+                        onChanged:_textFieldChanged,
+                        //用户完成内容
+                        onSubmitted:(text){},
+                        // 内容提交(按回车键)
+                        onEditingComplete:(){},
+                        decoration: InputDecoration(
+                            // 初始化的输入框比较高,这个解决高度
+                          contentPadding: EdgeInsets.all(10.0),
+                          labelText: "输入",
+                          helperText: "请输入你的输入",
+                          border: OutlineInputBorder(
+                            // 圆边角
+                            borderRadius: BorderRadius.circular(15.0)
+                          )
+                        ),
+                    ),
+                  )
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
