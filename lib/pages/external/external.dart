70c56f8f9a4bb061365cf1454a81382bf9550697
wangzhongjie <uvdream@163.com>
2019-07-05 12:30:39 +0800

    测试

--- lib/pages/external/external.dart
+++ lib/pages/external/external.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
 class ExternalPage extends StatelessWidget {
+  String _currentAnimation = "normal";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("第三方组件示例"),
       ),
-      body: Text("第三方"),
+      body: Container(
+        child: GestureDetector(
+          onTap: () {
+            if (_currentAnimation == "normal") {
+              setState(() {
+                _currentAnimation = "tap";
+              });
+            }
+          },
+          child: FlareActor(
+            "flrs/animation_test.flr",
+            animation: _currentAnimation,
+            fit: BoxFit.contain,
+            callback: (animationName){
+              switch(animationName){
+                case "tap":
+                  break;
+                case "success":
+                  break;
+                case "fail":
+                  break;
+              }
+            },
+          ),
+        )
+      )
     );
   }
 }
