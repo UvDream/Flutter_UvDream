e7317fab3fb4afc39e7b4ef046d5bc8e74a1edd5
wangzhongjie <uvdream@163.com>
2019-07-05 14:36:21 +0800

    flare动画

---
+++ lib/pages/external/Flare_status.dart
@@ -0,0 +1,51 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+class FlareStatus extends StatefulWidget {
+  @override
+  _FlareStatusState createState() => _FlareStatusState();
+}
+
+class _FlareStatusState extends State<FlareStatus> {
+  String _currentAnimation = "normal";
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(title: Text("第三方")),
+        body: Container(
+          child: GestureDetector(
+            child: FlareActor('flrs/animation_test.flr',
+                animation: _currentAnimation,
+                fit: BoxFit.contain, callback: (animationName) {
+              switch (animationName) {
+                case "tap":
+                  setState(() {
+                    _currentAnimation = "loading";
+                  });
+                  break;
+                case "success":
+                  setState(() {
+                    _currentAnimation = "normal";
+                  });
+                  break;
+                case "fail":
+                  setState(() {
+                    _currentAnimation = "normal";
+                  });
+                  break;
+              }
+            }),
+            onTap: () {
+              if (_currentAnimation == "normal") {
+                setState(() {
+                  _currentAnimation = "tap";
+                });
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
