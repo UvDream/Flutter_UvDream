d4050514121a1cd0bafbab777f4abd50baaa3bdf
wangzhongjie <uvdream@163.com>
2019-07-15 14:56:27 +0800

    [fix]文档更新

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 #Flutter_UvDream
 此项目主要代码演示各种常见的flutter组件
 
-[![](https://badge.juejin.im/entry/5c511725e51d4576563ac691/likes.svg?style=flat-square)](https://juejin.im/post/5c511154e51d456e413923bf)
-
 
 文档解释
 
-[flutter组件一期文档](https://segmentfault.com/a/1190000017928399)
-[flutter组件二期文档](https://segmentfault.com/a/1190000018024318)
-[flutter组件三期文档](https://segmentfault.com/a/1190000018065246)
+[Flutter组件一期文档](https://segmentfault.com/a/1190000017928399)
 
+[Flutter组件二期文档](https://segmentfault.com/a/1190000018024318)
 
+[Flutter组件三期文档](https://segmentfault.com/a/1190000018065246)
+
+[Flutter中使用阿里矢量图形](https://juejin.im/post/5d2c1a226fb9a07edf277515)
 
 
 
--- lib/pages/external/iconfont.dart
+++ lib/pages/external/iconfont.dart
@@ -5,36 +5,56 @@ class IconFontPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('IconFont'),
-        ),
-        body: SingleChildScrollView(
-          child: Wrap(
-            children: <Widget>[
-              Icon(
-                IconData(0xe64d,fontFamily: 'IconFont'),
-                size: 40,
-                color: Colors.red,
-              ),
-              Icon(
-                IconData(0xe650,fontFamily: 'IconFont'),
-                size: 40,
-                color: Colors.green,
-              ),
-              Icon(
-                IconData(0xe651,fontFamily: 'IconFont'),
-                size: 40,
-                color: Colors.blue,
-              ),
-              Icon(
-                IconData(0xe659,fontFamily: 'IconFont'),
-                size: 40,
-                color: Colors.black,
-              )
-            ],
+          appBar: AppBar(
+            title: Text('IconFont'),
           ),
-        )
-      ),
+          body: SingleChildScrollView(
+            child: Wrap(
+              alignment: WrapAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.all(5.0),
+                  child: Icon(
+                    IconData(0xe64d, fontFamily: 'IconFont'),
+                    size: 40,
+                    color: Colors.red,
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.all(5.0),
+                  child: Icon(
+                    IconData(0xe650, fontFamily: 'IconFont'),
+                    size: 40,
+                    color: Colors.green,
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.all(5.0),
+                  child: Icon(
+                    IconData(0xe651, fontFamily: 'IconFont'),
+                    size: 40,
+                    color: Colors.blue,
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.all(5.0),
+                  child: Icon(
+                    IconData(0xe659, fontFamily: 'IconFont'),
+                    size: 40,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
