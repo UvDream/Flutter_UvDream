194be131015e61e55bcfb9383f4fc21a22e68aab
wangzhongjie <uvdream@163.com>
2019-08-15 10:32:29 +0800

    [fix] 优化

--- lib/pages/external/flare/btn.dart
+++ lib/pages/external/flare/btn.dart
@@ -10,14 +10,14 @@ class _BtnFlareState extends State<BtnFlare> {
   String _currentAnimation = 'normal';
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: <Widget>[
         Container(
           width: 100,
           height: 100,
           child: GestureDetector(
             child: FlareActor(
-              "flrs/animation_test.flr",
+              "flrs/btn.flr",
               animation: _currentAnimation,
               fit: BoxFit.contain,
               callback: (animationName) {
@@ -34,40 +34,57 @@ class _BtnFlareState extends State<BtnFlare> {
             ),
           ),
         ),
-        RaisedButton(
-          color: Colors.green,
-          onPressed: () {
-            setState(() {
-              _currentAnimation = "success";
-            });
-          },
-          child: Text(
-            'Success',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        RaisedButton(
-          onPressed: () {
-            setState(() {
-              _currentAnimation = "tap";
-            });
-          },
-          child: Text(
-            'Tap',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        RaisedButton(
-          color: Colors.red,
-          onPressed: () {
-            setState(() {
-              _currentAnimation = "fail";
-            });
-          },
-          child: Text(
-            'fail',
-            style: TextStyle(color: Colors.white),
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              color: Colors.green,
+              onPressed: () {
+                setState(() {
+                  _currentAnimation = "success";
+                });
+              },
+              child: Text(
+                'Success',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                setState(() {
+                  _currentAnimation = "tap";
+                });
+              },
+              child: Text(
+                'Tap',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            RaisedButton(
+              color: Colors.orange,
+              onPressed: () {
+                setState(() {
+                  _currentAnimation = "loading";
+                });
+              },
+              child: Text(
+                'Loading',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            RaisedButton(
+              color: Colors.red,
+              onPressed: () {
+                setState(() {
+                  _currentAnimation = "fail";
+                });
+              },
+              child: Text(
+                'Fail',
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
         )
       ],
     );
